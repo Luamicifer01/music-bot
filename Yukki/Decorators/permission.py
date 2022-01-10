4ad122126a69c107803f14525085d781f5405f82
Luamicifer01 <97423418+Luamicifer01@users.noreply.github.com>
2022-01-10 10:02:39 +0530

    Update permission.py

--- Yukki/Decorators/permission.py
+++ Yukki/Decorators/permission.py
@@ -8,26 +8,26 @@ def PermissionCheck(mystic):
         a = await app.get_chat_member(message.chat.id, BOT_ID)
         if a.status != "administrator":
             return await message.reply_text(
-                "I need to be admin with some permissions:\n"
-                + "\n- **can_manage_voice_chats:** To manage voice chats"
-                + "\n- **can_delete_messages:** To delete Bot's Searched Waste"
-                + "\n- **can_invite_users**: For inviting assistant to chat."
+                "mereko permission mangta hai :\n"
+                + "\n- **can_manage_voice_chats:**voice chats chalane ke liye"
+                + "\n- **can_delete_messages:** tum log ka search hatane ke liye"
+                + "\n- **can_invite_users**: apne advik ko bulane ke liye *blush*"
             )
         if not a.can_manage_voice_chats:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
+                "mereko permission hi nahi diya *sad*."
                 + "\n**Permission:** __MANAGE VOICE CHATS__"
             )
             return
         if not a.can_delete_messages:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
+                "mereko permission hi nahi diya *sad*."
                 + "\n**Permission:** __DELETE MESSAGES__"
             )
             return
         if not a.can_invite_users:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
+                "mereko permission hi nahi diya *sad*."
                 + "\n**Permission:** __INVITE USERS VIA LINK__"
             )
             return
